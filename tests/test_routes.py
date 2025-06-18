5d0b99da0d3644f76747bbe5aff77f75322eb3bd
WAS4YH <was4yh@jp.bosch.com>
2025-06-18 19:51:26 -0400

    internal server error

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,6 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from unittest.mock import patch
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -187,3 +188,12 @@ class TestAccountService(TestCase):
         """It should return not allowed when trying to delete the base url"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+    def test_500_error_handler(self):
+        """It should return a server error, when an exceptino in the server is raised"""
+        app.config["TESTING"] = False
+        with patch("service.routes.Account.all", side_effect=Exception("Trigger 500")):
+            resp = self.client.get(BASE_URL)
+            self.assertEqual(resp.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        app.config["TESTING"] = True
